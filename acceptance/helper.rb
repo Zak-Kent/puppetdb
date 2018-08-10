801642cfb53f7fdedca71ac4106e3c7800b39401
Zachary Kent <zachary.kent@puppet.com>
2018-08-10 15:16:14 -0700

    (PDB-3702) Update behavior in acceptance tests
    Before this commit tests would always default to using the latest release
    of pdb regardless of the branch being tested against. This could cause
    situations where we would install a later version of pdb and then try to
    downgrade to an older version in the upgrade step. This adds branch specific
    behavior and accounts for newly supported platforms not having 4.2.3.8 builds.

--- acceptance/helper.rb
+++ acceptance/helper.rb
@@ -1,6 +1,7 @@
 require 'cgi'
 require 'beaker/dsl/install_utils'
 require 'beaker/dsl/helpers'
+require 'open3'
 require 'pp'
 require 'set'
 require 'test/unit/assertions'
@@ -263,6 +264,36 @@ module PuppetDBExtensions
   rescue RuntimeError => e
     display_last_logs(host)
     raise
+  end
+
+  def is_bionic()
+    return test_config[:os_families].has_key? 'ubuntu1804-64-1'
+  end
+
+  def oldest_supported
+    # account for special case where bionic doesn't have builds before 5.2.4
+    return is_bionic ? '5.2.4' : '4.2.3.8'
+  end
+
+  def get_testing_branch(version)
+    branch_name = version[0..3] + 'x'
+    if branch_name.chars.first != '5'
+      branch_name = 'master'
+    end
+    return branch_name
+  end
+
+  def get_latest_released(version)
+    cloned = system('git clone https://github.com/puppetlabs/puppetdb.git')
+    if cloned.nil?
+      raise 'error cloning puppetdb repo'
+    end
+    branch_name = get_testing_branch(version)
+    stdout, status = Open3.capture2('git', '-C', 'puppetdb', 'describe', '--tags', '--abbrev=0', "origin/#{branch_name}")
+    if status.exitstatus != 0
+      raise "error getting most recent tagged release. Status: #{status}"
+    end
+    return stdout.delete!("\n")
   end
 
   def get_package_version(host, version = nil)
--- acceptance/setup/pre_suite/70_add_dev_repo.rb
+++ acceptance/setup/pre_suite/70_add_dev_repo.rb
@@ -1,11 +1,15 @@
 repo_config_dir = 'tmp/repo_configs'
+#build_version = test_config[:install_mode] == :upgrade_oldest ? oldest_supported : test_config[:package_build_version]
 if (test_config[:install_type] == :package \
    and test_config[:package_build_version] \
    and not test_config[:skip_presuite_provisioning])
 then
   # do not install the dev_repo if a package_build_version has not been specified.
   databases.each do |database|
+    install_puppetlabs_dev_repo database, 'puppetdb', oldest_supported,
+      repo_config_dir, options
+
     install_puppetlabs_dev_repo database, 'puppetdb', test_config[:package_build_version],
-                                repo_config_dir, options
+      repo_config_dir, options
   end
 end
